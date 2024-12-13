b0f90cd2f839d4922941bf5358eaaf396545cc24
CodeName-maru <jason423@naver.com>
2024-12-13 10:21:22 +0900

    Initial commit

---
+++ src/main/java/com/madfllix/lastdance/LastDanceApplication.java
@@ -0,0 +1,13 @@
+package com.madfllix.lastdance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastDanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastDanceApplication.class, args);
+    }
+
+}
