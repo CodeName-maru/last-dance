10f0b3d110970c43b5cf3396c59374645c279ccc
CodeName-maru <jason423@naver.com>
2024-12-13 10:47:58 +0900

    커밋 테스트1

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,4 +1,12 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello!!!!!!!!!!!!";
+    }
 }
